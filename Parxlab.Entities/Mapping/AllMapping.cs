e67097f29cda7bfe50bd1e6c385fad141b2873ac
Sinan Can Soysal <sinancsoysal@gmail.com>
2021-10-11 10:06:13 +0300

    General mapping configurer added.

---
+++ Parxlab.Entities/Mapping/AllMapping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parxlab.Entities.Mapping
+{
+    class AllMapping
+    {
+        public static void AddCustomMapping(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new SensorMapping());
+        }
+    }
+}
