e84a819c71fe30cd789f9c8c0784d03f0547175b
Sinan Can Soysal <sinancsoysal@gmail.com>
2021-10-11 10:01:34 +0300

    Base mapping finished.

---
+++ Parxlab.Entities/Mapping/BaseMapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parxlab.Entities.Mapping
+{
+    class BaseMapping
+    {
+        public class BaseMapping : IEntityTypeConfiguration<BaseEntity>
+        {
+            public void Configure(EntityTypeBuilder<BaseEntity> builder)
+            {
+                builder.Property(p => p.CreatedDate).HasColumnType("datetime2").HasDefaultValueSql("getdate()");
+                builder.Property(p => p.ModifiedDate).HasColumnType("datetime2").HasDefaultValueSql("getdate()");
+            }
+        }
+    }
+}
