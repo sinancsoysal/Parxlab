51a6b4ad7754d7803df1d21cb5724930eb3a5c1f
Sinan Can Soysal <sinancsoysal@gmail.com>
2021-10-15 16:30:42 +0300

    15102021

--- ParxlabClientUI/Form1.Designer.cs
+++ ParxlabClientUI/Form1.Designer.cs
@@ -1,17 +1,16 @@
-﻿
-namespace ParxlabClientUI
+﻿namespace ParxlabClientUI
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// 必需的设计器变量。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// 清理所有正在使用的资源。
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,21 +20,224 @@ namespace ParxlabClientUI
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Windows 窗体设计器生成的代码
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ethConfiggroupBox = new System.Windows.Forms.GroupBox();
+            this.listenstatelabel = new System.Windows.Forms.Label();
+            this.deviceIPstatelabel = new System.Windows.Forms.Label();
+            this.startListenbutton = new System.Windows.Forms.Button();
+            this.deviceIPlabel = new System.Windows.Forms.Label();
+            this.connectstatelabel = new System.Windows.Forms.Label();
+            this.label87 = new System.Windows.Forms.Label();
+            this.severPorttextBox = new System.Windows.Forms.TextBox();
+            this.severPortlabel = new System.Windows.Forms.Label();
+            this.severIPcomboBox = new System.Windows.Forms.ComboBox();
+            this.severIPlabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ethConfiggroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ethConfiggroupBox
+            // 
+            this.ethConfiggroupBox.Controls.Add(this.listenstatelabel);
+            this.ethConfiggroupBox.Controls.Add(this.deviceIPstatelabel);
+            this.ethConfiggroupBox.Controls.Add(this.startListenbutton);
+            this.ethConfiggroupBox.Controls.Add(this.deviceIPlabel);
+            this.ethConfiggroupBox.Controls.Add(this.connectstatelabel);
+            this.ethConfiggroupBox.Controls.Add(this.label87);
+            this.ethConfiggroupBox.Controls.Add(this.severPorttextBox);
+            this.ethConfiggroupBox.Controls.Add(this.severPortlabel);
+            this.ethConfiggroupBox.Controls.Add(this.severIPcomboBox);
+            this.ethConfiggroupBox.Controls.Add(this.severIPlabel);
+            this.ethConfiggroupBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ethConfiggroupBox.ForeColor = System.Drawing.Color.Black;
+            this.ethConfiggroupBox.Location = new System.Drawing.Point(9, 12);
+            this.ethConfiggroupBox.Name = "ethConfiggroupBox";
+            this.ethConfiggroupBox.Size = new System.Drawing.Size(649, 101);
+            this.ethConfiggroupBox.TabIndex = 8;
+            this.ethConfiggroupBox.TabStop = false;
+            this.ethConfiggroupBox.Text = "Ethernet  Port Set";
+            // 
+            // listenstatelabel
+            // 
+            this.listenstatelabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.listenstatelabel.AutoSize = true;
+            this.listenstatelabel.Font = new System.Drawing.Font("SimSun", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listenstatelabel.ForeColor = System.Drawing.Color.DarkGray;
+            this.listenstatelabel.Location = new System.Drawing.Point(484, 30);
+            this.listenstatelabel.Name = "listenstatelabel";
+            this.listenstatelabel.Size = new System.Drawing.Size(34, 24);
+            this.listenstatelabel.TabIndex = 6;
+            this.listenstatelabel.Text = "●";
+            // 
+            // deviceIPstatelabel
+            // 
+            this.deviceIPstatelabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.deviceIPstatelabel.AutoSize = true;
+            this.deviceIPstatelabel.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deviceIPstatelabel.Location = new System.Drawing.Point(103, 76);
+            this.deviceIPstatelabel.Name = "deviceIPstatelabel";
+            this.deviceIPstatelabel.Size = new System.Drawing.Size(48, 17);
+            this.deviceIPstatelabel.TabIndex = 14;
+            this.deviceIPstatelabel.Text = "0.0.0.0";
+            // 
+            // startListenbutton
+            // 
+            this.startListenbutton.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startListenbutton.Location = new System.Drawing.Point(524, 29);
+            this.startListenbutton.Name = "startListenbutton";
+            this.startListenbutton.Size = new System.Drawing.Size(93, 29);
+            this.startListenbutton.TabIndex = 10;
+            this.startListenbutton.Text = "Start  Listen";
+            this.startListenbutton.UseVisualStyleBackColor = true;
+            this.startListenbutton.Click += new System.EventHandler(this.startListenbutton_Click);
+            // 
+            // deviceIPlabel
+            // 
+            this.deviceIPlabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.deviceIPlabel.AutoSize = true;
+            this.deviceIPlabel.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deviceIPlabel.Location = new System.Drawing.Point(43, 76);
+            this.deviceIPlabel.Name = "deviceIPlabel";
+            this.deviceIPlabel.Size = new System.Drawing.Size(57, 17);
+            this.deviceIPlabel.TabIndex = 13;
+            this.deviceIPlabel.Text = "WDC  IP:";
+            // 
+            // connectstatelabel
+            // 
+            this.connectstatelabel.AutoSize = true;
+            this.connectstatelabel.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectstatelabel.ForeColor = System.Drawing.Color.Black;
+            this.connectstatelabel.Location = new System.Drawing.Point(335, 76);
+            this.connectstatelabel.Name = "connectstatelabel";
+            this.connectstatelabel.Size = new System.Drawing.Size(116, 17);
+            this.connectstatelabel.TabIndex = 12;
+            this.connectstatelabel.Text = "Waiting  Connect...";
+            // 
+            // label87
+            // 
+            this.label87.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label87.AutoSize = true;
+            this.label87.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label87.Location = new System.Drawing.Point(238, 76);
+            this.label87.Name = "label87";
+            this.label87.Size = new System.Drawing.Size(94, 17);
+            this.label87.TabIndex = 11;
+            this.label87.Text = "Connect  State:";
+            // 
+            // severPorttextBox
+            // 
+            this.severPorttextBox.Font = new System.Drawing.Font("Calibri", 10.5F);
+            this.severPorttextBox.Location = new System.Drawing.Point(338, 30);
+            this.severPorttextBox.Name = "severPorttextBox";
+            this.severPorttextBox.Size = new System.Drawing.Size(129, 25);
+            this.severPorttextBox.TabIndex = 9;
+            this.severPorttextBox.Text = "6000";
+            // 
+            // severPortlabel
+            // 
+            this.severPortlabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.severPortlabel.AutoSize = true;
+            this.severPortlabel.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.severPortlabel.Location = new System.Drawing.Point(254, 35);
+            this.severPortlabel.Name = "severPortlabel";
+            this.severPortlabel.Size = new System.Drawing.Size(78, 17);
+            this.severPortlabel.TabIndex = 8;
+            this.severPortlabel.Text = "Server  Port:";
+            // 
+            // severIPcomboBox
+            // 
+            this.severIPcomboBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.severIPcomboBox.Font = new System.Drawing.Font("Calibri", 10.5F);
+            this.severIPcomboBox.FormattingEnabled = true;
+            this.severIPcomboBox.Items.AddRange(new object[] {
+            "192.168.1.2",
+            "119.78.248.11",
+            "192.168.1.103",
+            "192.168.100.106"});
+            this.severIPcomboBox.Location = new System.Drawing.Point(106, 30);
+            this.severIPcomboBox.Name = "severIPcomboBox";
+            this.severIPcomboBox.Size = new System.Drawing.Size(129, 25);
+            this.severIPcomboBox.TabIndex = 7;
+            // 
+            // severIPlabel
+            // 
+            this.severIPlabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.severIPlabel.AutoSize = true;
+            this.severIPlabel.Font = new System.Drawing.Font("Calibri", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.severIPlabel.Location = new System.Drawing.Point(35, 35);
+            this.severIPlabel.Name = "severIPlabel";
+            this.severIPlabel.Size = new System.Drawing.Size(65, 17);
+            this.severIPlabel.TabIndex = 6;
+            this.severIPlabel.Text = "Server  IP:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox1.Location = new System.Drawing.Point(6, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(640, 348);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(9, 119);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(649, 375);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Raw Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(662, 506);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ethConfiggroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parxlab Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ethConfiggroupBox.ResumeLayout(false);
+            this.ethConfiggroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox ethConfiggroupBox;
+        private System.Windows.Forms.Label listenstatelabel;
+        private System.Windows.Forms.Label deviceIPstatelabel;
+        private System.Windows.Forms.Button startListenbutton;
+        private System.Windows.Forms.Label deviceIPlabel;
+        private System.Windows.Forms.Label connectstatelabel;
+        private System.Windows.Forms.Label label87;
+        private System.Windows.Forms.TextBox severPorttextBox;
+        private System.Windows.Forms.Label severPortlabel;
+        private System.Windows.Forms.ComboBox severIPcomboBox;
+        private System.Windows.Forms.Label severIPlabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- ParxlabClientUI/Form1.cs
+++ ParxlabClientUI/Form1.cs
@@ -7,14 +7,387 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ParkingSystem;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 namespace ParxlabClientUI
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// <remarks>Ethernet State</remarks>
+        /// </summary>
+        private bool ethNetOpenState = false;
+        /// <summary>
+        /// <remarks>TCP Instantiation</remarks>
+        /// </summary>
+        public static ParkingSystem.ParkingRemoteTCP wapper = new ParkingRemoteTCP();
+        /// <summary>
+        /// <remarks>tcp Listener</remarks>
+        /// </summary>
+        private static TcpListener tcpListener = null;
+        /// <summary>
+        /// <remarks>IP Address</remarks>
+        /// </summary>
+        private static IPAddress localIP;
+        /// <summary>
+        /// <remarks>TCP portNum</remarks>
+        /// </summary>
+        private static UInt16 portNum;
+        /// <summary>
+        /// <remarks>Tcp Client</remarks>
+        /// </summary>
+        private static TcpClient client = new TcpClient();
+        /// <summary>
+        /// <remarks>Tcp Thread</remarks>
+        /// </summary>
+        private Thread m_serverThread;
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EthCommInit();
+            ParkingOriginalPacket.EvProcessReceivedPacket += sp_ProcessReceivedPacket;
+        }
+
+        #region Device Operation
+        
+        /// <summary>
+        /// Get device name
+        /// </summary>
+        /// <param name="devname">devicetype</param>
+        /// <returns>devicename</returns>
+        public string GetDevName(byte byteName)
+        {
+            string devname = "";
+            switch (byteName)
+            {
+                case 0x01:
+                    devname = "WDC-4003";
+                    break;
+                case 0x02:
+                    devname = "WDC-4005";
+                    break;
+                case 0x03:
+                    devname = "WDC-4008";
+                    break;
+                case 0x04:
+                    devname = "WDC-4007";
+                    break;
+                case 0x05:
+                    devname = "WPSD-340S3";
+                    break;
+                case 0x06:
+                    devname = "WPSD-340S5";
+                    break;
+                case 0x07:
+                    devname = "WPSD-340S8";
+                    break;
+                case 0x08:
+                    devname = "WPSD-340S7";
+                    break;
+                case 0x09:
+                    devname = "WPSD-340E3";
+                    break;
+                case 0x0A:
+                    devname = "WPSD-340E5";
+                    break;
+                case 0x0B:
+                    devname = "WPSD-340E8";
+                    break;
+                case 0x0C:
+                    devname = "WPSD-340E7";
+                    break;
+
+                default:
+                    devname = "WDC-400x";
+                    break;
+            }
+            return devname;
+        }
+        #endregion
+
+        #region Ethernet Operation
+        /// <summary>
+        /// Ethernet Initialize
+        /// </summary>
+        private void EthCommInit()
+        {
+            string addresses = GetLocalAddresses();
+            severIPcomboBox.Items.Clear();
+            if (addresses.Length > 0)
+            {
+
+                severIPcomboBox.Items.Add(addresses);
+
+                severIPcomboBox.Text = (string)severIPcomboBox.Items[0];
+            }
+        }
+        /// <summary>
+        /// Get Local IP Address
+        /// </summary>
+        public string GetLocalAddresses()
+        {
+            // 获取主机名
+            string strHostName = Dns.GetHostName();
+            System.Net.IPAddress addr;
+            addr = new System.Net.IPAddress(Dns.GetHostByName(Dns.GetHostName()).AddressList[0].Address);
+            return addr.ToString();
+        }
+        /// <summary>
+        /// Set Ethernet Close
+        /// </summary>
+        private void eth_Setclose()
+        {
+            connectstatelabel.Text = "Waiting  Connect...";
+            ethNetOpenState = false;
+            listenstatelabel.ForeColor = Color.DarkGray;
+            startListenbutton.Text = "Start  Listen";
+            deviceIPstatelabel.Text = "";
+
+        }
+        /// <summary>
+        /// startListenbutton_Click Oper
+        /// </summary>
+        /// /// <param name="sender">sender</param>
+        /// <param name="e">e</param>
+        private void startListenbutton_Click(object sender, EventArgs e)
+        {
+            deviceIPstatelabel.Text = "";
+            try
+            {
+                if (severPorttextBox.Text != "")
+                {
+                    if (startListenbutton.Text == "Start  Listen")
+                    {
+                        portNum = UInt16.Parse(severPorttextBox.Text);
+                        if (portNum > 0 && portNum < 65535)
+                        {
+                            TCP_StartListen();
+                            ethNetOpenState = true;
+                            startListenbutton.Text = "Stop  Listen";
+                            connectstatelabel.Text = "Listening...";
+                        }
+                        else
+                        {
+                            MessageBox.Show("Port Range:1~65535!");
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        try
+                        {
+                            if (connectstatelabel.Text == "Listening...")
+                            {
+                                tcpListener.Stop();
+                            }
+                            else
+                            {
+                                tcpListener.Stop();
+                                m_serverThread.Abort();
+                                m_serverThread = null; //中止线程
+                                
+                                client.Close();
+                            }
+                            eth_Setclose();
+                        }
+                        catch (SocketException ex)
+                        {
+                            MessageBox.Show("TCP Server Listen Error!" + ex.Message);
+                        }
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please input Port Adress!");
+                }
+            }
+            catch (ThreadAbortException exl)
+            {
+                MessageBox.Show("TCP Server Button Error:" + exl.ToString());
+            }
+            catch (SocketException se)           //处理异常
+            {
+                MessageBox.Show("TCP Server Listen Error:" + se.Message);
+            }
+            catch (Exception ex)
+            {
+                tcpListener.Stop();
+                client.Close();
+                MessageBox.Show("TCP Server Button Error:" + ex.ToString());
+            }
+        }
+        /// <summary>
+        /// Start TCP Listening
+        /// </summary>
+        public void TCP_StartListen()
+        {
+            try
+            {
+                localIP = IPAddress.Parse(this.severIPcomboBox.Text);
+                tcpListener = new TcpListener(localIP, portNum);
+                tcpListener.Start();
+                m_serverThread = new Thread(new ThreadStart(ReceiveAccept));
+                m_serverThread.Start();
+                m_serverThread.IsBackground = true;
+
+            }
+            catch (SocketException ex)
+            {
+                tcpListener.Stop();
+                eth_Setclose();
+                MessageBox.Show("TCP Server Listen Error:" + ex.Message);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("TCP Server Listen Error:" + err.Message);
+            }
+        }
+        /// <summary>
+        /// TCP Client Accept
+        /// </summary>
+        private void ReceiveAccept()
+        {
+            while (true)
+            {
+                try
+                {
+                    client = tcpListener.AcceptTcpClient();
+                    this.Invoke((EventHandler)delegate
+                    {
+                        this.listenstatelabel.ForeColor = Color.Lime;
+                        this.connectstatelabel.Text = "connect ok";
+                        this.deviceIPstatelabel.Text = client.Client.RemoteEndPoint.ToString();
+                        ethNetOpenState = true;
+                    });
+                    wapper = new ParkingRemoteTCP(client);
+                }
+                catch (Exception ex)
+                {
+                    //eth_Setclose();
+                    // MessageBox.Show(ex.Message, "?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        #endregion
+
+        #region Received Process
+        /// <summary>
+        /// <remarks>Process Received Packet</remarks>
+        /// </summary>
+        /// <param name="pk">Received Packet</param>
+        private void sp_ProcessReceivedPacket(baseReceivedPacket pk)
+        {
+            try
+            {
+                byte revType = Convert.ToByte(pk.type_ver >> 8);
+                string wpsdid = "";
+                string WDCid = "";
+                string RSSI = "";
+                byte carState = 0;
+                string voltage = "";
+                string hardVer = "";
+                string softVer = "";
+                string deviceName = "";
+                string hbPeriod = "";
+                this.Invoke((EventHandler)delegate
+                {
+                    #region Senser Heart Beat
+                    if (pk is SensorHBeat)
+                       {
+                           SensorHBeat hb = (SensorHBeat)pk;
+                            reshow(hb.recRawData,true);
+                            wpsdid = (hb.WPSD_ID).ToString("X2").PadLeft(8, '0');
+                            WDCid = (hb.WDC_ID).ToString("X2").PadLeft(8, '0');
+                            softVer = "v" + int.Parse(hb.APP_VER.ToString("X2").Substring(0, 1)).ToString() + "." + int.Parse(hb.APP_VER.ToString("X2").Substring(1, 1)).ToString().PadLeft(2, '0');
+                            hardVer = ((int)(hb.HARD_VER) + 10).ToString();
+                            hardVer = "v" + hardVer.Substring(0, 1) + "." + hardVer.Substring(1, 1);
+                            voltage = (Math.Round((decimal)hb.VOLT / 10, 2)).ToString()+"V"; RSSI = ((Int16)hb.RSSI - 30).ToString();
+                            hbPeriod = hb.HB_PERIOD.ToString();
+                            deviceName = GetDevName(hb.DEV_TYPE);
+                            carState = hb.CAR_STATE;
+                            if (carState == 0x01)
+                            {
+                                richTextBox1.AppendText("wpsd id:" + wpsdid + "\nsoft Ver:" + softVer + "\nhard Ver:" + hardVer + "\nvoltage:" + voltage + "\nRSSI:" + RSSI + "\ncar State:Have Car\n");
+                            }
+                            else
+                            {
+                                richTextBox1.AppendText("wpsd id:" + wpsdid + "\nsoft Ver:" + softVer + "\nhard Ver:" + hardVer + "\nvoltage:" + voltage + "\nRSSI:" + RSSI + "\ncar State:No Car\n");
+                            } 
+                    
+                    }
+                      #endregion
+                    #region Senser Detect
+                    else if (pk is SensorDetect)
+                    {
+                        SensorDetect decbeat = (SensorDetect)pk;
+                        reshow(decbeat.recRawData, true);
+                        wpsdid = (decbeat.WPSD_ID).ToString("X2").PadLeft(8, '0');
+                        WDCid = (decbeat.WDC_ID).ToString("X2").PadLeft(8, '0');
+                        hardVer = ((int)(decbeat.HARD_VER) + 10).ToString();
+                        hardVer = "v" + hardVer.Substring(0, 1) + "." + hardVer.Substring(1, 1);
+                        deviceName = GetDevName(decbeat.DEV_TYPE);
+                        carState = decbeat.CAR_STATE;
+                        if (carState == 0x01)
+                        {
+                            richTextBox1.AppendText("wpsd id:" + wpsdid + "\nhard Ver:" + hardVer + "\ncar State:Have Car\n"); 
+                        }
+                        else
+                        {
+                            richTextBox1.AppendText("wpsd id:" + wpsdid + "\nhard Ver:" + hardVer + "\ncar State:No Car\n");
+                        }    
+                    }
+                    #endregion
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        /// <summary>
+        /// <remarks>Received Data Show</remarks>
+        /// </summary>
+        /// <param name="text">Received data</param>
+        public void reshow(byte[] text,bool source)
+        {
+            string restr = "";
+            if (text != null)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    restr += text[i].ToString("X2");
+                    restr += " ";
+                }
+            }
+            if (source)
+            {
+                richTextBox1.AppendText(System.DateTime.Now.ToString() + "[Received]:  " + restr + "\n");
+            }
+            else { richTextBox1.AppendText(System.DateTime.Now.ToString() + "[Send]:  " + restr + "\n"); }
+        }
+        
+        #endregion
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+
+                System.Environment.Exit(System.Environment.ExitCode);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+       
     }
 }
