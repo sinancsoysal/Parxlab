a03517cfca18b715ee029527ad1b00e05436964c
sinan can soysal <sinancsoysal@gmail.com>
2021-11-10 02:23:41 +0300

    10112021

---
+++ Parxlab.Repository/Repositories/InvoiceRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using Parxlab.Data;
+using Parxlab.Entities;
+using Parxlab.Repository.Interfaces;
+
+namespace Parxlab.Repository.Repositories
+{
+    public class InvoiceRepository:Repository<Invoice>,IInvoiceRepository
+    {
+        public InvoiceRepository(ApplicationDbContext context, IDbConnection connection) : base(context, connection)
+        {
+        }
+    }
+}
