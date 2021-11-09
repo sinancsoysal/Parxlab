1c619cfeb5ce2b5b45ee572ba760795eeeb2e4ee
sinan can soysal <sinancsoysal@gmail.com>
2021-11-10 02:24:42 +0300

    10112021-2

---
+++ Parxlab.Entities/Mapping/InvoiceMapping.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Parxlab.Entities.Mapping
+{
+   public class InvoiceMapping:BaseEntityTypeConfiguration<Invoice>
+    {
+        public override void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            base.Configure(builder);
+
+        }
+    }
+}
